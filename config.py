d30d1f4af12642bd1098a5d90aa0541c2c8b56d2
Oxblad <33175965+Oxblad@users.noreply.github.com>
2022-01-26 22:43:52 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 import os
-db_link = 'postgres://vcyhcmiixkuimc:1645563ab53478f879dbefab83ba0b770289f5176da9ae170e995a885a219962@ec2-52-19-164-214.eu-west-1.compute.amazonaws.com:5432/d969233cistfqr'
+db_link = os.environ.get("DATABASE_URL")
