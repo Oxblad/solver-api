b91ab6f82fbcf8264297652168659130fd7a41b7
Oxblad <33175965+Oxblad@users.noreply.github.com>
2022-01-27 19:34:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_answer(text):
         print(item.question) 
         text = text.replace("%20", " ")
         print(f"ATr {text}")
-        if item.question == text and jellyfish.jaro_distance(text, item.question) >= item.floating_point:    
+        if item.question == text or jellyfish.jaro_distance(text, item.question) >= item.floating_point:    
             return jsonify({'answer': item.answer, "is_clown" : item.is_clown, "type" : item.type, "floating_point": item.floating_point}) 
     return "No"
 
